80e9d62aa1deca2e175097af47ea1bdb7f92c782
AbeerAhmad2003 <abeerahmad159357@gmail.com>
2025-12-19 23:03:44 +0200

    add Exceptions

---
+++ AccommodationBookingPlatform.Application/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+﻿namespace AccommodationBookingPlatform.Application.Exceptions
+{
+    public class ForbiddenException : Exception
+    {
+        public ForbiddenException(string message)
+            : base(message)
+        {
+        }
+    }
+}
